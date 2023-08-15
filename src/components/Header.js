c834a446f0681cc125a5dc9370e583569c3a6d2b
Sai Kumar Majji <saikumar170325@gmail.com>
2023-08-15 19:41:52 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+             <img className="h_image" src = "../images/troll.png" alt = "troll" />
+             <h2 className="h_title">Meme Generator</h2>
+             <h4 className="h_project"> React Course - Project 3</h4>
+        </header>
+    )
+}
